72b9addfb5c11c95eaed1a2bdf204d6e71e077e0
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2023-04-02 00:08:37 +0530

    fix port

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from src import init_app
 import os
 
+app = init_app()
 if __name__ == "__main__":
-    app = init_app()
-    app.run(port=int(os.environ.get("PORT", 5000)),host='0.0.0.0')
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0')
