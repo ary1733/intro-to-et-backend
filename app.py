0ec9a0673ecd44c12706ede78b83f51b86e07dc6
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2023-04-02 20:45:23 +0530

    add cors

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from src import init_app
 import os
-from flask_cors import cross_origin
+from flask_cors import CORS
 
 app = init_app()
-
-@app.route("/")
-@cross_origin()
-def helloWorld():
-  return "Hello, cross-origin-world!"
-
+CORS(app,resources={r'*':{'origins':'*','supports_credentials':True}})
 if __name__ == "__main__":
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0')
