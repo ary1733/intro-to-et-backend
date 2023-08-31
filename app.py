177c7e2502cd2ef83f70ef17a04390a195beef13
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2023-08-31 10:46:36 +0530

    add readme

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ import os
 app = init_app()
 
 if __name__ == "__main__":
+    # this port 8080 will not be used if you run through gunicorn
+    # use --bind=0.0.0.0:8080 as defualt port is 8000
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0')
